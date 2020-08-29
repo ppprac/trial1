6505be1666c8092548e56be094e03001dded360c
ppprac <70270759+ppprac@users.noreply.github.com>
2020-08-29 12:59:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# trial1
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,40 @@
+var rect1, rect2;
+
+function setup() 
+{
+  createCanvas(800,400);
+ rect1 =  createSprite(400, 200, 50, 50);
+ rect2 = createSprite(100,200, 80, 80)
+ rect1.shapeColor="yellow";
+ rect2.shapeColor="green";
+ rect1.debug=true;
+ rect2.debug=true;
+}
+
+function draw() 
+{
+  background(0,0,255);  
+  rect2.x=World.mouseX;
+  rect2.y=World.mouseY;
+  if(rect2.x - rect1.x < rect2.width/2 + rect1.width/2 && rect1.x - rect2.x < rect2.width/2 + rect1.width/2)
+  {
+    rect1.shapeColor="red";
+    rect2.shapeColor="red";
+  }
+  else
+  {
+    rect1.shapeColor = "green";
+    rect2.shapeColor = 'green';
+  }
+  if(rect2.y - rect1.y < rect2.height/2 + rect1.height/2 && rect1.y - rect2.y < rect2.height/2 + rect1.height/2) 
+  {
+    rect1.shapeColor="red";
+    rect2.shapeColor="red";
+  }
+  else
+  {
+    rect1.shapeColor = "green";
+    rect2.shapeColor = 'green';
+  }
+  drawSprites();
+}
